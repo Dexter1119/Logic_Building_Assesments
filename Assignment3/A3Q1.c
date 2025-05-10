4e2d2c01f3d03e532912768eab5f7e41346ab9e4
Pradhumnya C. Kalsait <kalsaitpc@gmail.com>
2025-05-10 15:29:35 +0530

    Update A3Q1.c

--- Assignment3/A3Q1.c
+++ Assignment3/A3Q1.c
@@ -2,7 +2,7 @@
 //////////////////////////////////////////////////////////////////////////
 // 
 // Name:        A3Q1.c
-// Description: Program to print the number is even 
+// Description: Program to print the n even numbers
 // Author:      Pradhumnya Changdev Kalsait
 // Assignment:  Assignement 3 Question 1
 // Date:        09-05-25
