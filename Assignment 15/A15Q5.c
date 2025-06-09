d2b4a21ec74fb59c534c1e30fa954ac385a7f01f
Pradhumnya C. Kalsait <kalsaitpc@gmail.com>
2025-06-09 23:54:23 +0530

    Add files via upload

---
+++ Assignment 15/A15Q5.c
@@ -0,0 +1,62 @@
+///////////////////////////////////////////////////////////////////
+// 
+// Name:        A15Q5.c
+// Description: Problems on Patern Printing  
+// Author:      Pradhumnya Changdev Kalsait.
+// Date:        05/06/25
+//
+///////////////////////////////////////////////////////////////////
+
+
+// Input : iRow = 4 iCol = 4
+// Output : 1 2 3 4
+//          1 * * 4
+//          1 * * 4
+//          1 2 3 4
+
+
+#include<stdio.h>
+
+
+void Pattern(int iRow , int iCol)
+{   
+    if(iRow != iCol)
+    {
+        printf("!!...The Number of Rows n Column should equal...!!");
+        return;
+    }
+    int i = 0 , j = 0;
+    for(i = 1 ; i <= iRow ; i++)
+    {
+        for(j = 1 ; j <= iCol ; j++)
+        {
+            if(i == 1 || j == 1 || i == iRow || j == iCol)
+            {
+                printf("%d\t",j);
+            }
+            else
+            {
+                printf("*\t");
+            }
+            
+        }
+        printf("\n");
+    }
+    
+}
+
+int main()
+{
+
+    int iValue1 = 0 , iValue2 = 0;
+    printf("ENter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("ENter the number of col : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+
+    return 0;
+}
