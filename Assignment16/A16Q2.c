a33c7166358e7749fa365ee96c26e3348095b850
Pradhumnya C. Kalsait <kalsaitpc@gmail.com>
2025-06-10 00:00:57 +0530

    Add files via upload

---
+++ Assignment16/A16Q2.c
@@ -0,0 +1,57 @@
+///////////////////////////////////////////////////////////////////
+// 
+// Name:        A16Q2.c
+// Description: Problems on Patern Printing  
+// Author:      Pradhumnya Changdev Kalsait.
+// Date:        05/06/25
+//
+///////////////////////////////////////////////////////////////////
+
+
+// Input : iRow = 4 iCol = 4
+// Output : * * * *
+//          * * *
+//          * *
+//          *
+
+
+#include<stdio.h>
+
+
+void Pattern(int iRow , int iCol)
+{   
+    if(iRow != iCol)
+    {
+        printf("!!...The Number of Rows n Column should equal...!!");
+        return;
+    }
+    int i = 0 , j = 0;
+    for(i = 1 ; i <= iRow ; i++)
+    {
+        for(j = iCol ; j >= 1 ; j--)
+        {
+            if(i <= j )
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+    
+}
+
+int main()
+{
+
+    int iValue1 = 0 , iValue2 = 0;
+    printf("ENter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("ENter the number of col : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+
+    return 0;
+}
