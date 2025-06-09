8e791d25e0015e9e1c95bd63cdd9317fa0d9380c
Pradhumnya C. Kalsait <kalsaitpc@gmail.com>
2025-06-09 23:52:46 +0530

    Add files via upload

---
+++ Assignement 12/A12Q1.c
@@ -0,0 +1,42 @@
+///////////////////////////////////////////////////////////////////
+// 
+// Name:        A12Q1.c
+// Description: Problems on Patern Printing  
+// Author:      Pradhumnya Changdev Kalsait.
+// Date:        05/06/25
+//
+///////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0 , j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+
+    int iValue1 = 0 , iValue2 = 0;
+    printf("ENter the number of rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("ENter the number of col : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+
+    return 0;
+}
